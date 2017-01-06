157c55657406baff7fe16c07f4cf7b10fb5797c4
Andrew Michitson <michitson@yahoo.com>
2017-01-06 22:23:22 +0000

    init commit

---
+++ app/app.jsx
@@ -0,0 +1,19 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var {Route, Router, IndexRoute, hashHistory} = require('react-router');
+//var Main = require('Main');
+//var Timer = require('Timer');
+//var Countdown = require('Countdown');
+
+//load foundation
+//require('style!css!foundation-sites/dist/css/foundation.min.css')
+// moved to webpack config
+//require('style!css!foundation-sites/dist/foundation.min.css')
+$(document).foundation();
+
+require('style!css!sass!applicationStyles')
+
+ReactDOM.render(
+<P>Boilerplate 3 project</P>
+    //document.getElementById("app")
+);
